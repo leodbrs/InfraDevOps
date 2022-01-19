66c6aada063360341c004c6c4e3335d1dc6779e4
Léo <leodb.scap@gmail.com>
2022-01-19 19:37:36 +0100

    Mise en place d'un container de test :wrench:

--- data/app/includes/infoConnexion.php
+++ data/app/includes/infoConnexion.php
@@ -2,5 +2,5 @@
 $user = "postgres"; // nom utilisateur sur la base de donnée
 $pass = "root"; // mot de passe du l'utilisateur
 $dbname = "postgres"; // nom de la base de donnée
-$lehost = "10.108.8.28"; //adresse du serveur postgresql
+$lehost = "database"; //adresse du serveur postgresql
 $leport = "5432"; // port de ce serveur
--- test/flash-get-ip-of-container/web/app.py
+++ test/flash-get-ip-of-container/web/app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask import request
 
+import requests
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    curl "http://database/"
-    return request.data, 200
+    response = requests.get("http://database/")
+    return response.data
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
