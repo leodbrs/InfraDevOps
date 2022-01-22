6cceb5214b8c0ef9adff76bd83a84c0265bf75fe
Léo <leodb.scap@gmail.com>
2022-01-22 17:51:14 +0100

    Changement d'info de connexion db

--- data/app/includes/infoConnexion.php
+++ data/app/includes/infoConnexion.php
@@ -1,6 +1,6 @@
 <?php
-$user = "postgres"; // nom utilisateur sur la base de donnée
-$pass = "root"; // mot de passe du l'utilisateur
-$dbname = "postgres"; // nom de la base de donnée
+$user = "dvdrental"; // nom utilisateur sur la base de donnée
+$pass = 'Pa$$word'; // mot de passe du l'utilisateur
+$dbname = "dvdrental"; // nom de la base de donnée
 $lehost = "database"; //adresse du serveur postgresql
 $leport = "5432"; // port de ce serveur
