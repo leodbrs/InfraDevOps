a0340690137e7f2ff9b59cbca1b38fb7fff517b5
BuildTools <leodb.scap@gmail.com>
2022-03-17 15:17:15 +0100

    Removing WebApp from this repo

--- data/app/includes/infoConnexion.php
+++
@@ -1,6 +0,0 @@
-<?php
-$user = "dvdrental"; // nom utilisateur sur la base de donnée
-$pass = 'Pa$$word'; // mot de passe du l'utilisateur
-$dbname = "dvdrental"; // nom de la base de donnée
-$lehost = "database"; //adresse du serveur postgresql
-$leport = "5432"; // port de ce serveur
--- data/app/scripts/connexion.js
+++
@@ -1,9 +0,0 @@
-function afficherPassword() {
-            var x = document.getElementById("motdepasse");
-            if (x.type === "password") {
-                x.type = "text";
-            }
-            else {
-                x.type = "password"
-            }
-        }
